e2de28fad2f12253c42582ca91ff82c5a4767f50
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-07-25 10:50:00 -0400

    Update WingerGradeStreamlit.py

--- WingerGradeStreamlit.py
+++ WingerGradeStreamlit.py
@@ -104,11 +104,11 @@ def WingerFunction(dataframe):
         date = more_data['As At Date']
         
         if team_name in ['Boston Bolts U13', 'Boston Bolts U14']:
-            wing_df = pd.read_csv("PostMatchReviewApp_v2/Thresholds/WingerThresholds1314.csv")
+            wing_df = pd.read_csv("Thresholds/WingerThresholds1314.csv")
         elif team_name in ['Boston Bolts U15', 'Boston Bolts U16']:
-            wing_df = pd.read_csv("PostMatchReviewApp_v2/Thresholds/WingerThresholds1516.csv")
+            wing_df = pd.read_csv("Thresholds/WingerThresholds1516.csv")
         elif team_name in ['Boston Bolts U17', 'Boston Bolts U19']:
-            wing_df = pd.read_csv("PostMatchReviewApp_v2/Thresholds/WingerThresholds1719.csv")
+            wing_df = pd.read_csv("Thresholds/WingerThresholds1719.csv")
 
         mean_values = wing_df.iloc[0, 0]
         std_values = wing_df.iloc[1, 0]
