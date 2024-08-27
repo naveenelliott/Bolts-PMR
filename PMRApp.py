33224a4bbb1055820b18f01a8a7d651c010da406
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-08-26 20:54:21 -0400

    Update PMRApp.py

--- PMRApp.py
+++ PMRApp.py
@@ -27,7 +27,6 @@ combined_df = getting_PSD_lineup_data()
 combined_df['Starts'] = combined_df['Starts'].astype(float)
 combined_df['Date'] = pd.to_datetime(combined_df['Date'])
 combined_df['Date'] = combined_df['Date'].dt.strftime('%m/%d/%Y')
-st.write(combined_df)
 
 combined_df = combined_df.loc[combined_df['Team Name'].isin(bolts_allowed) & combined_df['Opposition'].isin(opp_allowed)].reset_index(drop=True)
 
