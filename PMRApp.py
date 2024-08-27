5117b1520b3850069b68b7080e66a720540c03df
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-08-26 20:43:42 -0400

    Update PMRApp.py

--- PMRApp.py
+++ PMRApp.py
@@ -44,26 +44,45 @@ st.title("Bolts Post-Match Review App")
 
 st.markdown("Select the Team, Opponent, and Date (Optional) to See the Post-Match Review")
 
+# Selecting the Bolts team
 teams = list(combined_df['Team Name'].unique())
-teams.sort()
-if "selected_team" not in st.session_state:
-    st.session_state['selected_team'] = teams[0]
-selected_team = st.selectbox('Choose the Bolts MLS Next Team:', teams, index=teams.index(st.session_state['selected_team']))
-combined_df = combined_df.loc[combined_df['Team Name'] == selected_team]
 
+selected_team = st.session_state.get('selected_team', teams[0])
+if selected_team not in teams:
+    selected_team = teams[0]  # Default to the first date if not found
+
+selected_team = st.selectbox('Choose the Bolts Team:', teams, index=teams.index(selected_team))
+st.session_state['selected_team'] = selected_team
+
+# Filtering based on the selected team
+combined_df = combined_df.loc[combined_df['Team Name'] == st.session_state['selected_team']]
+
+# Selecting the opponent team
 opps = list(combined_df['Opposition'].unique())
-if "selected_opp" not in st.session_state:
-    st.session_state["selected_opp"] = opps[0] 
-selected_opp = st.selectbox('Choose the Opposition:', opps, index=opps.index(st.session_state["selected_opp"]))
-combined_df = combined_df.loc[combined_df['Opposition'] == selected_opp]
 
-combined_df['Date'] = pd.to_datetime(combined_df['Date'])
-combined_df['Date'] = combined_df['Date'].dt.strftime('%m/%d/%Y')
-date = list(combined_df['Date'].unique())
-if 'date' not in st.session_state:
-    st.session_state['selected_date'] = date[0]
-selected_date = st.selectbox('Choose the Date (if necessary)', date, index=date.index(st.session_state['selected_date']))
-combined_df = combined_df.loc[combined_df['Date'] == selected_date]
+selected_opp = st.session_state.get('selected_opp', opps[0])
+if selected_opp not in opps:
+    selected_opp = opps[0]  # Default to the first date if not found
+selected_opp = st.selectbox('Choose the Opposition:', opps, index=opps.index(selected_opp))
+st.session_state['selected_opp'] = selected_opp
+
+# Filtering based on the selected opponent
+combined_df = combined_df.loc[combined_df['Opposition'] == st.session_state['selected_opp']]
+
+# Selecting the date
+dates = list(combined_df['Date'].unique())
+
+# Check if the selected date in the session state exists in the list of dates
+selected_date = st.session_state.get('selected_date', dates[0])
+if selected_date not in dates:
+    selected_date = dates[0]  # Default to the first date if not found
+
+# Create the selectbox for the date
+selected_date = st.selectbox('Choose the Date (if necessary)', dates, index=dates.index(selected_date))
+st.session_state['selected_date'] = selected_date
+
+# Filtering based on the selected date
+combined_df = combined_df.loc[combined_df['Date'] == st.session_state['selected_date']]
 
 # Initialize prev_player in session state if not already present
 
