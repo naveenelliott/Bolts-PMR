aa2102cff907c66231cd749de0dbd6f782132f4c
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-09-01 12:16:06 -0400

    Update 4GK_Report.py

--- pages/4GK_Report.py
+++ pages/4GK_Report.py
@@ -651,7 +651,7 @@ if not pd.isna(gk_info['Vasily Notes']).any() and not gk_info.empty:
     # Step 2: Filter all_games_gk by these combinations
     end_overall = all_games_gk.merge(unique_combinations, on=['Team Name', 'Opposition', 'Match Date'], how='inner')
     end_overall = end_overall.loc[end_overall['Player Full Name'] == gk_name]
-    end_overall = end_overall[['Player Full Name', 'Team Name', 'Opposition', 'Match Date', 'Save Held', 'Save Parried', 'Goal Against', 'Progr Regain ', 
+    end_overall = end_overall[['Player Full Name', 'Team Name', 'Opposition', 'Match Date', 'mins played', 'Save Held', 'Save Parried', 'Goal Against', 'Progr Regain ', 
                             'Pass Completion ', 'Opp Effort on Goal', 'Progr Pass Completion ', 'Successful Cross', 'Unsucc cross GK', 'Hands GK', 'Unsucc Hands', 'Ground GK', 'Unsucc Ground']]
     end_overall['Total CC'] = end_overall['Successful Cross'] + end_overall['Unsucc cross GK']
     end_overall['Cross %'] = (end_overall['Successful Cross']/end_overall['Total CC']) * 100
