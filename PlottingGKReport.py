008d1dd025b2ee189dab67a5f2f245f9369c1811
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-09-01 12:10:05 -0400

    Update PlottingGKReport.py

--- PlottingGKReport.py
+++ PlottingGKReport.py
@@ -290,9 +290,8 @@ def gettingGameGrade(dataframe):
     # Apply the function to the 'team_name' column
     pd_df['bolts team'] = pd_df['bolts team'].apply(rearrange_team_name)
     pd_df = pd_df.loc[(pd_df['bolts team'] == team) & (pd_df['start_time'] == match_date)]
-    st.write(pd_df)
     pd_df = pd_df.loc[pd_df['athlete_name'] == pname]
-
+    st.write(pd_df)
     
     avg_u13 = 2.8
     avg_u14 = 3.0
