deaa055dfaa7b4166f0e249f4b0490fd5e9f69d5
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-09-01 18:12:04 -0400

    Update 2Post-Match Review.py

--- pages/2Post-Match Review.py
+++ pages/2Post-Match Review.py
@@ -1694,8 +1694,8 @@ fig1.update_layout(
     annotations=[
         dict(
             text="Click the top right of chart to see the shots better",
-            x=0.35,
-            y=0.85,  # Adjust this value to position the subtitle correctly
+            x=0.4,
+            y=1.15,  # Adjust this value to position the subtitle correctly
             xref="paper",
             yref="paper",
             showarrow=False,
