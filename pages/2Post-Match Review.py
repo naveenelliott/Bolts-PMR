99c153acd27ecd27f3356eb10625a1882fe51071
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-09-08 14:45:15 -0400

    Update 2Post-Match Review.py

--- pages/2Post-Match Review.py
+++ pages/2Post-Match Review.py
@@ -350,7 +350,6 @@ final_grade_df = temp_df.copy()
 chances_created.fillna(0, inplace=True)
 st.write(chances_created)
 
-st.write(select_event_df)
 
 for index, row in final_grade_df.iterrows():
     if row['Position'] == 'ATT':
@@ -359,6 +358,7 @@ for index, row in final_grade_df.iterrows():
         temp_event_df = temp_event_df[wanted]
         select_temp_df = select_event_df.loc[select_event_df['Player Full Name'] == row['Player Name']]
         select_temp_df = select_temp_df[wanted]
+        st.write(select_temp_df)
         select_temp_df = StrikerEventFunction(temp_event_df, select_temp_df)
         final_grade_df.at[index, 'Final Grade'] = row['Final Grade'] + ((select_temp_df.at[0, 'Finishing'])*.2)
     elif (row['Position'] == 'RW') or (row['Position'] == 'LW'):
