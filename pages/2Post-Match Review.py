537a19a02e029b77e5f1cd3cb9c482ada19f38f9
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-09-08 15:26:32 -0400

    Update 2Post-Match Review.py

--- pages/2Post-Match Review.py
+++ pages/2Post-Match Review.py
@@ -358,10 +358,8 @@ for index, row in final_grade_df.iterrows():
         wanted = ['xG + xA', 'Team']
         temp_event_df = temp_event_df[wanted]
         select_temp_df = select_event_df.loc[select_event_df['Player Full Name'] == row['Player Name']]
-        st.write(select_temp_df)
         select_temp_df = select_temp_df[wanted]
         select_temp_df = StrikerEventFunction(temp_event_df, select_temp_df)
-        st.write(select_temp_df)
         final_grade_df.at[index, 'Final Grade'] = row['Final Grade'] + ((select_temp_df.at[0, 'Finishing'])*.2)
     elif (row['Position'] == 'RW') or (row['Position'] == 'LW'):
         temp_event_df = chances_created.loc[(chances_created['Primary Position'] == 'LW') | (chances_created['Primary Position'] == 'RW')]
@@ -379,6 +377,7 @@ for index, row in final_grade_df.iterrows():
         select_temp_df = select_event_df.loc[select_event_df['Player Full Name'] == row['Player Name']]
         select_temp_df = select_temp_df[wanted]
         select_temp_df = CMEventFunction(temp_event_df, select_temp_df)
+        st.write(select_temp_df)
         final_grade_df.at[index, 'Final Grade'] = row['Final Grade'] + ((select_temp_df.at[0, 'Playmaking'])*.2)
 
 
