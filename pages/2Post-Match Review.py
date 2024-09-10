80a9f17eb5dbee816fc60fa6e0751e281ffa5330
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-09-09 20:06:25 -0400

    Update 2Post-Match Review.py

--- pages/2Post-Match Review.py
+++ pages/2Post-Match Review.py
@@ -1551,7 +1551,7 @@ fig1.add_trace(go.Scatter(
         
 
 
-custom_order = ['Shot', 'Blocked', 'SOT', 'Goal']
+custom_order = ['Shot', 'Blocked', 'SOT', 'SOT Far Post', 'SOT Inside Post', 'Goal', 'Goal Far Post', 'Goal Inside Post']
 xg_data['Event'] = pd.Categorical(xg_data['Event'], categories=custom_order, ordered=True)
 xg_data = xg_data.sort_values('Event')
 
