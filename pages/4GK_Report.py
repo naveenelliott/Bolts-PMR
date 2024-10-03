97e40b5508dae342ebadee3c1c94aa3f19657321
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-10-03 10:56:09 -0400

    Update 4GK_Report.py

--- pages/4GK_Report.py
+++ pages/4GK_Report.py
@@ -648,6 +648,7 @@ if not pd.isna(gk_info['Vasily Notes']).any() and not gk_info.empty:
     game_grade_end = end_overall.copy()
     end_overall.drop(columns=['Save Held', 'Save Parried', 'Successful Cross', 'Unsucc cross GK', 'Total CC'], inplace=True)
     end_overall.rename(columns={'Team Name': 'Team'}, inplace=True)
+    st.write(end_overall)
     game_grade_end.rename(columns={'Team Name': 'Team'}, inplace=True)
 
     if not summary_df.empty:
