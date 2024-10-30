447abb6a151c0942f2667b243ea98a2da9fc81aa
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-10-30 18:26:55 -0400

    Update 2Post-Match Review.py

--- pages/2Post-Match Review.py
+++ pages/2Post-Match Review.py
@@ -751,6 +751,8 @@ if flag == 1:
     else:
         st.write(closest_game)
         closest_game_index = compare_opps.index(closest_game)
+        st.write(closest_game_index)
+        st.write(compare_opp)
     
     with col3:
         compare_opp = st.selectbox('Choose the Comparison Game:', compare_opps, index=closest_game_index)
