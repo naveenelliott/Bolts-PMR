79bca82d7ef8662913207f8f680889f6c6a37c4d
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-12-08 12:02:11 -0500

    Update 2Post-Match Review.py

--- pages/2Post-Match Review.py
+++ pages/2Post-Match Review.py
@@ -437,8 +437,17 @@ else:
             st.write(select_temp_df)
             final_grade_df.at[index, 'Final Grade'] = row['Final Grade'] + ((select_temp_df.at[0, 'Finishing'])*.2)
         elif (row['Position'] == 'RW') or (row['Position'] == 'LW'):
+            our_player_data = player_data.loc[player_data['Player Full Name'] == player_name]
+            st.write(our_player_data)
+            select_temp_df = WingerSOTFunction(our_player_data)
+            st.write(select_temp_df)
+            final_grade_df.at[index, 'Final Grade'] = row['Final Grade'] + ((select_temp_df.at[0, 'Finishing'])*.2)
         elif (row['Position'] == 'CM') or (row['Position'] == 'RM') or (row['Position'] == 'LM') or (row['Position'] == 'AM'):
-    
+            our_player_data = player_data.loc[player_data['Player Full Name'] == player_name]
+            st.write(our_player_data)
+            select_temp_df = CMSOTFunction(our_player_data)
+            st.write(select_temp_df)
+            final_grade_df.at[index, 'Final Grade'] = row['Final Grade'] + ((select_temp_df.at[0, 'Finishing'])*.2)
 
 
 final_grade_df['Final Grade'] = final_grade_df['Final Grade'] + final_grade_df['Adjustments']
