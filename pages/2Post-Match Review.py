fc335bc4c27be5237619086d9508ae9f172c9791
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-12-22 09:29:55 -0500

    Update 2Post-Match Review.py

--- pages/2Post-Match Review.py
+++ pages/2Post-Match Review.py
@@ -570,7 +570,9 @@ for player_name, group in grouped:
         ax.text(10, 77.5, position['Player Full Name'][0], color='black', size=6, ha='center', va='center')
     elif group['Position Tag'].iloc[0] == 'CM':
         position = combined_df.loc[combined_df['Position Tag'] == 'CM'].reset_index()
-        position['Player Full Name'] = position['Player Full Name'].apply(lambda x: x.split(' ', 1)[1])
+        position['Player Full Name'] = position['Player Full Name'].apply(
+            lambda x: x.split(' ', 1)[1] if x != 'Ferreira' else x
+        )
         if (len(position) == 1):
             # cm
             circle = Circle((40, 55), 7, edgecolor='black', facecolor='#6bb2e2')
