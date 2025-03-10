ea61cc0262bb441bfa2c596587981da0760893c9
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2025-03-10 10:25:56 -0400

    Update PositivesAndNegativesStreamlit.py

--- PositivesAndNegativesStreamlit.py
+++ PositivesAndNegativesStreamlit.py
@@ -35,6 +35,7 @@ def PositivesAndNegativesStreamlit(team_select, opp_select, date_select, comp_op
         # manually changing St Louis because weekly report and actions don't align
         overall.loc[overall['Opposition'] == 'St Louis', 'Date'] = '2023-12-09'
         overall['Date'] = pd.to_datetime(overall['Date']).dt.strftime('%m/%d/%Y')
+        further_df['Date'] = pd.to_datetime(further_df['Date']).dt.strftime('%m/%d/%Y')
         overall['Unique Opp and Date'] = overall['Opposition'] + ' (' + overall['Date'] + ')'
         overall.rename(columns={'Date': 'Match Date', 
                                    'Team Name': 'Team'}, inplace=True)
